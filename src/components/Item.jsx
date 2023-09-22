89915e2451aef81614646ab6b68b448988c09c65
양수진 <soojin9885@naver.com>
2023-09-22 14:47:28 +0900

    url 수정

---
+++ src/components/Item.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Item = ({imgSrc, name, position, index}) => {
+    /*
+    프로필 카드 클릭 시 해당하는 선수의 상세페이지로 이동
+    /detail/index
+    => useNavigate 사용
+    */
+   const nav = useNavigate();
+  return (
+    <div className='item-item'
+    onClick={()=>{
+
+        
+        console.log("Detail 요청")
+        let url = "http://15.164.171.89:8091/SpringBoot2/PlayerDetail.do?name="+name;
+
+        axios.get(url)
+            .then((res)=>{
+                console.log(res.data) // 나상호의 정보
+            });
+        
+        nav(`/detail/${index}`)}}>
+        <img src={imgSrc}/>
+        <table>
+            <tbody>
+                <tr>
+                    <td>이름</td>
+                    <td>{name}</td>
+                </tr>
+                <tr>
+                    <td>포지션</td>
+                    <td>{position}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Item
